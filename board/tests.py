2bbba2fa1c971d69df0a8be36827620378b87d6e
krartistjw <krartistjw@gmail.com>
2023-11-19 23:19:19 +0900

    test case add

--- board/tests.py
+++ board/tests.py
@@ -7,31 +7,26 @@ client = Client()
 
 
 class BoardCreateTest(TestCase):
-    # 테스트 함수는 test_ 를 붙여주어야 테스트 함수로 인식한다.
     def test_board_post_success(self):
         data = {
             'name': 'John',
             'content': 'somthing',
         }
-
         response = client.post('/board/', json.dumps(data), content_type='application/json')
-
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json(), {
             "message": "SUCCESS"
         })
 
     def test_board_get_list(self):
-
         response = client.get('/board/1/', {'name':'John','content':'something'})
-
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
             "message": "SUCCESS"
         })
 
 
-class BoardTestCase(TestCase):
+class BoardDetailTest(TestCase):
     def setUp(self):
         Board.objects.create(
             name='John',
