afd2fbfc3fb835d77f4ffa8507e85b6c2d5067a7
krartistjw <krartistjw@gmail.com>
2023-11-19 23:36:38 +0900

    test case

--- board/tests.py
+++ board/tests.py
@@ -6,24 +6,24 @@ from board.models import Board
 client = Client()
 
 
-class BoardCreateTest(TestCase):
-    def test_board_post_success(self):
-        data = {
-            'name': 'John',
-            'content': 'somthing',
-        }
-        response = client.post('/board/', json.dumps(data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json(), {
-            "message": "SUCCESS"
-        })
-
-    def test_board_get_list(self):
-        response = client.get('/board/1/', {'name':'John','content':'something'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {
-            "message": "SUCCESS"
-        })
+# class BoardCreateTest(TestCase):
+#     def test_board_post_success(self):
+#         data = {
+#             'name': 'John',
+#             'content': 'somthing',
+#         }
+#         response = client.post('/board/', json.dumps(data), content_type='application/json')
+#         self.assertEqual(response.status_code, 201)
+#         self.assertEqual(response.json(), {
+#             "message": "SUCCESS"
+#         })
+#
+#     def test_board_get_list(self):
+#         response = client.get('/board/1/', {'name':'John','content':'something'})
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.json(), {
+#             "message": "SUCCESS"
+#         })
 
 
 class BoardDetailTest(TestCase):
